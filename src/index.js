da7751d59d09c4574296543566b2c83391850c71
Christophe Hurpeau <christophe@hurpeau.com>
2016-03-15 23:05:12 +0100

    alp-node and alp-browser

--- src/index.js
+++
@@ -1,9 +0,0 @@
-Error.stackTraceLimit = Infinity; // TODO move somewhere else
-
-export function newController(actions) {
-    return actions;
-}
-
-newController.action = function (action) {
-    return action;
-};
